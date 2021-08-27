c9f9fa1824f56692f7aa9cb428f1a2d746039b9d
FusionSolutions <admin@fusionsolutions.io>
2021-08-27 12:00:57 +0200

    Revert "upload"
    This reverts commit ddd4ff3eb95b8dcafa0787cf6388ea0ef854ff50.

--- fsRPCClient/client.py
+++ fsRPCClient/client.py
@@ -81,7 +81,7 @@ class Client(T_Client):
 			self.log.debug("Protocol initialized")
 	def __del__(self) -> None:
 		self.close()
-	def __enter__(self) -> Client:
+	def __enter__(self) -> Any:
 		return self.clone()
 	def __exit__(self, type:Any, value:Any, traceback:Any) -> None:
 		self.close()
@@ -300,6 +300,7 @@ class Client(T_Client):
 				self._parseResult(resp[0], resp[1], pickle.loads(decompress(resp[2])), "")
 			else:
 				raise MessageError("Invalid payload")
+			
 	def _parseResult(self, id:Union[int, str], isSuccess:bool, result:Any, uid:str) -> None:
 		if id not in self.requests:
 			self.log.warn("Got unexpected result id: {}", id)
